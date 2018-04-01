1721e78c15e7e7aebfb9288bbf16210ac568ea1c
kash <kashiwa.electory@gmail.com>
2018-04-01 22:45:18 +0900

    whitespace

--- src/main/java/com/github/kasheight/Rires.java
+++ src/main/java/com/github/kasheight/Rires.java
@@ -4,28 +4,28 @@ import java.util.regex.*;
 
 public class Rires {
 
-	private static char getChar(char[] chararray) {
-		return chararray[(int)(Math.random() * 5)];
-	}
+    private static char getChar(char[] chararray) {
+        return chararray[(int)(Math.random() * 5)];
+    }
 
-	public static void main(String[] args) {
-	    final char[] characters = {'り', 'れ', 'す', 'し', 'ね'};
-	    int i = 0;
-	    String text = "";
-	    Pattern p = Pattern.compile("りれすしね");
-	    Matcher m;
+    public static void main(String[] args) {
+        final char[] characters = {'り', 'れ', 'す', 'し', 'ね'};
+        int i = 0;
+        String text = "";
+        Pattern p = Pattern.compile("りれすしね");
+        Matcher m;
 
-	    do {
-	    	char c = getChar(characters);
-	    	i++;
+        do {
+            char c = getChar(characters);
+            i++;
 
-	        while (text.length() < 5) {
+            while (text.length() < 5) {
                 text = text + getChar(characters);
             }
 
             System.out.print(c);
-	        text = text + c;
-	        m = p.matcher(text);
+            text = text + c;
+            m = p.matcher(text);
         } while (!m.find());
 
         System.out.println("");
